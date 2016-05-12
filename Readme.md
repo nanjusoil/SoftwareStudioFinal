a101d2b3993c9507c26acd6b5caf4d93e2b2386c
nanjusoil <nanjusoil@gmail.com>
2016-05-12 19:34:19 +0800

    Added room changing

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
-[Coding Styles](https://google.github.io/styleguide/javaguide.html "Coding Styles")
+#[Coding Styles](https://google.github.io/styleguide/javaguide.html)
 Please read the coding styles before you start
+#[ControlP5 -- a proccesing library](http://www.sojamo.de/libraries/controlP5/)
+Please include the ControlP5 jar
---
+++ src/main/java/MainApplet.java
@@ -0,0 +1,68 @@
+package main.java;
+
+import controlP5.ControlEvent;
+import controlP5.ControlP5;
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+
+@SuppressWarnings("serial")
+public class MainApplet extends PApplet{
+	private String path = "main/resources/";
+	private PImage imgBackground;
+	private final int windowWidth = 1200, windowHeight = 700;
+	private ControlP5 controlP5;
+	public void setup() {
+		size(windowWidth, windowHeight);
+		smooth();
+		imgBackground = loadImage(path+"LivingRoom.jpg");
+		imgBackground.resize(windowWidth, windowHeight);
+		controlP5 = new ControlP5(this);
+		controlP5.addButton("buttonLeft")
+		     .setPosition(0,275)
+		     .setImages(loadImage(path+"arrowLeft.png"), loadImage(path+"arrowLeft.png") , loadImage(path+"arrowLeftPressed.png"))
+		     .updateSize();
+		controlP5.addButton("buttonRight")
+	     .setPosition(700,275)
+	     .setImages(loadImage(path+"arrowRight.png"), loadImage(path+"arrowRight.png") , loadImage(path+"arrowRightPressed.png"))
+	     .updateSize();
+	}
+	
+	public void controlEvent(ControlEvent theEvent) {
+
+	}
+
+
+	public void buttonLeft(int theValue) {
+		imgBackground = loadImage(path+"Bedroom.jpg");
+		imgBackground.resize(windowWidth, windowHeight);
+	}
+	
+	public void buttonRight(int theValue) {
+		imgBackground = loadImage(path+"Kitchen.jpg");
+		imgBackground.resize(windowWidth, windowHeight);
+	}
+
+	public void draw() {
+		image(imgBackground, 0, 0);
+	}
+
+	public void mousePressed() {
+	}
+	
+	public void mouseDragged() {
+	}
+	
+	public void mouseReleased() {
+	}
+	
+	public void setAllNetwork()
+	{
+	}
+	
+	public void keyPressed(){
+		
+	}
+
+}
