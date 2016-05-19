0e467f4b71219d475214459989cf91e9541ae11d
martin427 <martintw.kuo@gmail.com>
2016-05-19 18:04:55 +0800

    server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(3001);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+  console.log("connection");
+  socket.emit('message', { hello: 'world' });
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', function (chunk) {
+	socket.emit('message', { hello: chunk });
+  });
+  process.stdin.on('end', () => {
+	process.stdout.write('end');
+  });
+  socket.on('message', function (data) {
+  console.log(data);
+  });
+});
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,23 @@
 package main.java;
+import java.net.URISyntaxException;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+
+import org.json.JSONObject;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 @SuppressWarnings("serial")
 public class Main extends JFrame{
 	
 	private final static int windowWidth = 1303, windowHeight = 745;
 	public static JFrame window;
-	
-	public static void main(String [] args){
+	private static Socket socket;
+	public static String sockettext = "acascascsacsacascacsacsacsacsacascsacsacsacsacsac";
+	public static void main(String [] args) throws URISyntaxException{
 		
 		window = new JFrame("±K«Ç°k²æ");
 		//MainApplet applet = new MainApplet(window);
@@ -20,5 +29,30 @@ public class Main extends JFrame{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(windowWidth, windowHeight);
 		window.setVisible(true);
+		socket = IO.socket("http://localhost:3001");
+		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+		  @Override
+		  public void call(Object... args) {
+			System.out.println("on");
+		  }
+
+		}).on("message", new Emitter.Listener() {
+
+		  @Override
+		  public void call(Object... args) {
+			  JSONObject jsonobject = (JSONObject)args[0];
+			  sockettext = jsonobject.getString("hello");
+			  System.out.println(sockettext);
+		  }
+
+		}).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+		  @Override
+		  public void call(Object... args) {System.out.println("off");}
+
+		});
+		socket.connect();
+
 	}
 }
