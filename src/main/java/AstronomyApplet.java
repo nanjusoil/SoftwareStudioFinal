8222796ad664b33502c6c816927f602d60a6239d
keineahnung2345 <johnlin840324@yahoo.com.tw>
2016-06-05 12:40:16 +0800

    AstronomyApplet: move the initialization of itemBox forward to avoid it covering the picture of mykey

--- src/main/java/AstronomyApplet.java
+++ src/main/java/AstronomyApplet.java
@@ -48,16 +48,8 @@ public class AstronomyApplet extends PApplet implements Runnable{
 	
 	// parameter::  sizex, sizey , x , y
 	private Item table;//200, 200, 0, 0
-	private Item lucifer;
-	private Item confucius;
-	private Item buddha;
-	private Item ludongbin;
 	
 	//the token which ...'s phrase is written on
-	private Item baijuyi;
-	private Item dufu;
-	private Item wangwei;
-	private Item libai;
 	
 	private ArrayList<Item> itemArr; 
 	//used as second parameter of checkItem(), to make different items' isHolded exclusive
@@ -84,10 +76,6 @@ public class AstronomyApplet extends PApplet implements Runnable{
 		loginapplet = new LoginApplet(jframe);
 		numSolved = 0;
 		
-		
-		
-		
-	
 		controlP5 = new ControlP5(this);
 		controlP5.addKnob(" ").setImage(loadImage(path+"arrowLeft.png"))
 		  .setViewStyle(1)
@@ -151,38 +139,35 @@ public class AstronomyApplet extends PApplet implements Runnable{
 				if (theEvent.getAction() == 100) {
 					imgBackground = loadImage(path+"BuddhistTempleWithoutTable.png");
 					imgBackground.resize(windowWidth-itemboxWidth, windowHeight);
-					baijuyi.solControlP5.setVisible(false);
-					dufu.solControlP5.setVisible(false);
-					wangwei.solControlP5.setVisible(false);
-					libai.solControlP5.setVisible(false);
-					table.controlP5.setVisible(true);
+					
 				}
 		    }
 		};
 		
 		
 
-		open = new Item(this ,140,100,300 ,550 , "safe_open2.png" , "safe_open2.png" , "safe_open2.png", 60, 60, 900, 600,  "cabinet.png", Type.TOOL){
+		open = new Item(this ,140,100,300 ,550 , "safe_open2.png" , "safe_open2.png" , "safe_open2.png", Type.FURNITURE){
 		@Override
 		public void controlEvent(CallbackEvent theEvent) {
-			if(theEvent.getController().getName().equals("mykey")){
+			if(theEvent.getController().getName().equals("safe_open2")){
 				if (theEvent.getAction() == 100) {
-					if(!isInBox){
-						itemBox.putinItem(this);
-					}else{
-						itemBox.checkItem(this, itemArr);
-					}
-				}
-			}else if(theEvent.getController().getName().equals("solmykey")){
-				if ((theEvent.getAction() == 100) && isInBox && isHolded){
-					itemBox.useItem(this);
+					
 				}
 			}
 	    }
 			
 		};
-		
-		
+		//new the itemBox earlier so it won't cover mykey
+		itemBox = new ItemBox(this, 1190, 0, "propsColumn.png"){
+			@Override
+			public void controlEvent(CallbackEvent theEvent) {
+				 
+					if (theEvent.getAction() == 100) {
+						mouseClicked();
+					}
+			    }
+		    
+		};
 		
 		
 		mykey = new Item(this , 40, 40, 350 , 580 , "mykey.png" , "mykey.png" , "mykey.png", 60, 60, 900, 600, "cabinet.png", Type.TOOL){
@@ -199,99 +184,58 @@ public class AstronomyApplet extends PApplet implements Runnable{
 				}else if(theEvent.getController().getName().equals("solmykey")){
 					if ((theEvent.getAction() == 100) && isInBox && isHolded){
 						itemBox.useItem(this);
+						this.controlP5.setVisible(false);
+						System.out.println("You open the door.");
 					}
 				}
 		    }
 		};
-		safe = new Item(this ,160,120,300,550 , "safe_nomove.png" , "safe_nomove.png" , "safe_nomove.png", 118 , 286 , 850 , 205, "safe_nomove.png", Type.FURNITURE){
+		
+		safe = new Item(this ,160,120,300,550 , "safe_nomove.png" , "safe_nomove.png" , "safe_nomove.png", Type.FURNITURE){
 			
 			@Override
 			public void controlEvent(CallbackEvent theEvent) {
 					
-				if(theEvent.getController().getName().equals("carpet")){
+				if(theEvent.getController().getName().equals("safe_nomove")){
 					if (theEvent.getAction() == 100) {
-						if(!isInBox){
-							itemBox.putinItem(this);
-						}else{
-							itemBox.checkItem(this, itemArr);
-						}
-					}
-				}else if(theEvent.getController().getName().equals("solcarpet")){
-					if ((theEvent.getAction() == 100) && isInBox && isHolded){ //later change
-						itemBox.useItem(this);
-						numSolved ++;
+						
 					}
 				}
-				
-				
 		   	}
 		};
+
 		
-		
-		
-		cassiopeia = new Item(this , 140 , 100 , 930 , 92 , "Cassiopeia.png" , "Cassiopeia.png" , "Cassiopeia.png", 118 , 186 , 750 , 105, "Cassiopeia.png", Type.FURNITURE)
+		cassiopeia = new Item(this , 140 , 100 , 930 , 92 , "Cassiopeia.png" , "Cassiopeia.png" , "Cassiopeia.png", Type.FURNITURE)
 		
 		{
 			
 			@Override
 				public void controlEvent(CallbackEvent theEvent) {
 						
-					if(theEvent.getController().getName().equals("carpet")){
+					if(theEvent.getController().getName().equals("Cassiopeia")){
 						if (theEvent.getAction() == 100) {
-							if(!isInBox){
-								itemBox.putinItem(this);
-							}else{
-								itemBox.checkItem(this, itemArr);
-							}
-						}
-					}else if(theEvent.getController().getName().equals("solcarpet")){
-						if ((theEvent.getAction() == 100) && isInBox && isHolded){ //later change
-							itemBox.useItem(this);
-							numSolved ++;
+							
 						}
 					}
-					
-					
 			   	}
 			
 		};
 		
-		bigDipper = new Item(this ,180 ,150  , 150 , 280 , "BigDipper.jpg" , "BigDipper.jpg" , "BigDipper.jpg", 118 , 286 , 850 , 205, "BigDipper.png", Type.FURNITURE){
+		bigDipper = new Item(this ,180 ,150  , 150 , 280 , "BigDipper.jpg" , "BigDipper.jpg" , "BigDipper.jpg", Type.FURNITURE){
 			
 			@Override
 				public void controlEvent(CallbackEvent theEvent) {
-					if(theEvent.getController().getName().equals("carpet")){
+					if(theEvent.getController().getName().equals("BigDipper")){
 						if (theEvent.getAction() == 100) {
-							if(!isInBox){
-								itemBox.putinItem(this);
-							}else{
-								itemBox.checkItem(this, itemArr);
-							}
-						}
-					}else if(theEvent.getController().getName().equals("solcarpet")){
-						if ((theEvent.getAction() == 100) && isInBox && isHolded){ //later change
-							itemBox.useItem(this);
-							numSolved ++;
+							
 						}
 					}
 			   	}
 		};
 		
-		itemBox = new ItemBox(this, 1190, 0, "propsColumn.png"){
-			@Override
-			public void controlEvent(CallbackEvent theEvent) {
-				 
-					if (theEvent.getAction() == 100) {
-						mouseClicked();
-					}
-			    }
-		    
-		};
-		
-		
-		
+	
 		mykey.controlP5.setVisible(false);
-		itemArr = new ArrayList<Item>(Arrays.asList(baijuyi, dufu, wangwei, libai, mykey));
+		itemArr = new ArrayList<Item>(Arrays.asList(mykey));
 		
 		
 	}
