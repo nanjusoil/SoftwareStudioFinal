31313bb2b53ef1b52e94e680eb8363f229f7aba8
keineahnung2345 <johnlin840324@yahoo.com.tw>
2016-06-07 02:35:34 +0800

    json

--- server/index.js
+++ server/index.js
@@ -30,7 +30,161 @@ io.on('connection', function (socket) {
   socket.on('message', function (data) {
   console.log(data);
   });
-  socket.on('safeopen', function (data) {
-	socket.broadcast.emit('safeopen', { hello: 'world' });
+  
+  socket.on('leftboxopen', function (data) {
+  	socket.broadcast.emit('leftboxopen', { hello: 'world' });
+  console.log(data);
   });
+
+  socket.on('rightboxopen', function (data) {
+  	socket.broadcast.emit('rightboxopen', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('putinCardbaijuyi', function (data) {
+  	socket.broadcast.emit('putinCardbaijuyi', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('checkCardbaijuyi', function (data) {
+  	socket.broadcast.emit('checkCardbaijuyi', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('useCardbaijuyi', function (data) {
+  	socket.broadcast.emit('useCardbaijuyi', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('putinCarddufu', function (data) {
+  	socket.broadcast.emit('putinCarddufu', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('checkCarddufu', function (data) {
+  	socket.broadcast.emit('checkCarddufu', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('useCarddufu', function (data) {
+  	socket.broadcast.emit('useCarddufu', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('putinCardwangwei', function (data) {
+  	socket.broadcast.emit('putinCardwangwei', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('checkCardwangwei', function (data) {
+  	socket.broadcast.emit('checkCardwangwei', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('useCardwangwei', function (data) {
+  	socket.broadcast.emit('useCardwangwei', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('putinCardlibai', function (data) {
+  	socket.broadcast.emit('putinCardlibai', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('checkCardlibai', function (data) {
+  	socket.broadcast.emit('checkCardlibai', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('useCardlibai', function (data) {
+  	socket.broadcast.emit('useCardlibai', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('putinItemmykey', function (data) {
+  	socket.broadcast.emit('putinItemmykey', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('checkItemmykey', function (data) {
+  	socket.broadcast.emit('checkItemmykey', { hello: 'world' });
+  console.log(data);
+  });  
+
+  socket.on('useItemmykey', function (data) {
+  	socket.broadcast.emit('useItemmykey', { hello: 'world' });
+  console.log(data);
+  });
+
+  socket.on('nextRoom', function (data) {
+  	socket.broadcast.emit('nextRoom', { hello: 'world' });
+  console.log(data);
+  });
+ 
+  socket.on('safeopen', function (data) {
+	  	socket.broadcast.emit('safeopen', { hello: 'world' });
+	  console.log(data);
+  });
+  
+  socket.on('faithPlay', function (data) {
+	  	socket.broadcast.emit('faithPlay', { hello: 'world' });
+	  console.log(data);
+  });
+  
+  socket.on('heroPlay', function (data) {
+	  	socket.broadcast.emit('heroPlay', { hello: 'world' });
+	  console.log(data);
+  });
+  
+  socket.on('vocalPlay', function (data) {
+	  	socket.broadcast.emit('vocalPlay', { hello: 'world' });
+	  console.log(data);
+  });
+  
+  socket.on('ruralsPlay', function (data) {
+	  	socket.broadcast.emit('ruralsPlay', { hello: 'world' });
+	  console.log(data);
+  });
+
+  socket.on('faithStop', function (data) {
+	  	socket.broadcast.emit('faithStop', { hello: 'world' });
+	  console.log(data);
+  });
+  
+  socket.on('vocalStop', function (data) {
+	  	socket.broadcast.emit('vocalStop', { hello: 'world' });
+	  console.log(data);
 });
+
+  socket.on('heroStop', function (data) {
+	  	socket.broadcast.emit('heroStop', { hello: 'world' });
+	  console.log(data);
+});
+
+  socket.on('ruralsStop', function (data) {
+	  	socket.broadcast.emit('ruralsStop', { hello: 'world' });
+	  console.log(data);
+});
+
+  socket.on('putinItempaperball', function (data) {
+	  	socket.broadcast.emit('putinItempaperball', { hello: 'world' });
+	  console.log(data);
+});
+
+  socket.on('checkItempaperball', function (data) {
+	  	socket.broadcast.emit('checkItempaperball', { hello: 'world' });
+	  console.log(data);
+});
+  
+  socket.on('safeopenastro', function (data) {
+	  	socket.broadcast.emit('safeopenastro', { hello: 'world' });
+	  console.log(data);
+});
+
+
+});
+
+
+
+
+
